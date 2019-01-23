0e02534415922dd2498fa1f154ada814a91c849f
Max Cramer <maximiliancramer@yahoo.com>
2019-01-23 11:02:22 +0000

    created youtube component

---
+++ src/Youtube.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Youtube extends Component{
+  render(){
+    return(
+      <div>
+        I am from Youtube
+      </div>
+    )
+  }
+}
+
+
+export default Youtube;
